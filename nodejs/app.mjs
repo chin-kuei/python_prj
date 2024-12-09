c39839d22273a8a5b6d92e71e12fbabe76aec816
chin-kuei <chao.chin-kuei@hotmail.com>
2024-12-09 16:23:27 +0800

    launch.json

---
+++ nodejs/app.mjs
@@ -0,0 +1,9 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    res.end('hello world');
+});
+
+server.listen(3000, () => {
+    console.log('server is running at http://localhost:3000');
+});
